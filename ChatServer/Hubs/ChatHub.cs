68dbbdc920e45ae393ff29d12bf085c954f6fc02
Adam <Adam@TROLL-PC>
2017-02-16 00:30:08 +0100

    7. Select friend to send message

---
+++ ChatServer/Hubs/ChatHub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ChatServer.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private UserRepository _userRepository = new UserRepository();
+        //Doupravit tak, aby šĺo chatovat pouze s přátelama
+        public bool CheckContact(string nick)
+        {
+            User user = this._userRepository.FindByNick(nick);
+            if (user != null)
+                return true;
+            return false;
+        }
+        public void SendMessage(string ConnectionID, string message, string nickFriend)
+        {
+            DateTime dateNow = DateTime.Now;
+        }
+    }
+}
